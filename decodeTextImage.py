3ce439bcc0587830fc80265d7f041e59f9627162
jsaysay <jsaysay5@gmail.com>
2017-10-31 13:07:02 -0700

    Add files via upload

---
+++ decodeTextImage.py
@@ -0,0 +1,57 @@
+"""
+Name:Jonathan Saysay
+CWID: 891681553
+Class: CS353
+
+Description - 	This program will extract the text from an 
+				Image and print it to the terminal.
+				see readme.txt file for use.
+"""
+from PIL import Image
+import sys
+import math
+
+imageName = sys.argv[1]
+#open image
+img = Image.open(imageName)
+#convert image pixels into 1d array of tuples (R,G,B).
+flatImage = img.getdata()
+#index of last pixel of the image
+lastIndex = len(flatImage)
+#variable to store string representation of data size.
+strSize = ""
+
+#get size of message by reading the bottom right 11 pixels
+for i in reversed(range(lastIndex - 11, lastIndex)):
+	for j in range(3):
+		if not((i == lastIndex - 11) and (j == 2)):
+			strSize += str(flatImage[i][j] % 2)
+
+#convert string representation of data size to Int
+dataSize = (int(strSize,2))
+#number of pixels the data is embedded in.
+pixelNumber = math.ceil(dataSize/3)
+
+#indexes where the data is stored.
+dataEndIndex = lastIndex - 11
+dataBeginIndex = dataEndIndex - pixelNumber 
+
+#store each 8 bits of data in as a string.
+eightBitStrData = ""
+#variable to store all the data.
+finalMessage = ""
+#count to keep track of each 8 bits of data
+count = 0
+
+#extract message from pixels
+for i in reversed(range(dataBeginIndex,dataEndIndex)):
+	for j in range(3):
+		eightBitStrData += str(flatImage[i][j] % 2)
+		count += 1
+		if count == 8:
+			finalMessage += chr(int(eightBitStrData,2))
+			count = 0
+			eightBitStrData = ""
+
+print(finalMessage)
+img.close()
---
+++ encodeTextImage.py
@@ -0,0 +1,103 @@
+"""
+Name:Jonathan Saysay
+CWID: 891681553
+Class: CS353
+
+Description - 	This program will embed text into an image
+				see readme.txt file for use.
+"""
+
+from PIL import Image
+import sys
+import math
+
+imageName = sys.argv[1]
+imageData = sys.stdin.read()
+#open image
+img = Image.open(imageName)
+#get data size
+imageDataSize = len(imageData) * 8
+#flatImage will contain all pixels in an array.
+flatImage = img.getdata()
+endIndex =  len(flatImage)
+#variable for holding 32 bit binary representation of data size.
+strData = ""
+#convert size of data from int to 32 bit string representation of size.
+strData = bin(imageDataSize).lstrip("-0b").zfill(32)
+#dataArr will contain all the pixels of the image with embedded text and size.
+dataArr = []
+#pixelArr is a temporary variable for holding 1 pixel at a time.
+pixelArr = []
+#index for the 32 bit string.
+index = 0
+
+#embed text size into bottom right 11 pixels.
+for i in reversed(range(endIndex - 11, endIndex)):
+	for j in range(3):
+		if not((i == endIndex - 11) and (j == 2)):
+			if strData[index] == "0":
+				if flatImage[i][j] % 2 == 1:
+					pixelArr.append(flatImage[i][j] - 1)
+				else:
+					pixelArr.append(flatImage[i][j])
+			else:
+				if flatImage[i][j] % 2 == 0:
+					pixelArr.append(flatImage[i][j] + 1)
+				else:
+					pixelArr.append(flatImage[i][j])
+		else:
+			pixelArr.append(flatImage[i][j])
+
+		index += 1
+
+	dataArr.append((pixelArr[0],pixelArr[1],pixelArr[2]))
+	pixelArr.clear()
+
+#amount of pixels needed to embed data.
+pixelNumber =  math.ceil(imageDataSize/3)
+
+#indexes from  the 12th pixel to pixel based on data size.
+dataEndIndex = endIndex - 11
+dataBeginIndex = dataEndIndex - pixelNumber
+
+#store binary representation for a character of the data.
+charData = bin(ord(imageData[0])).lstrip("0b").zfill(8)
+dataIndex = 0
+charIndex = 0
+
+#embed message data onto pixels starting with 12th pixel.
+for i in reversed(range(dataBeginIndex,dataEndIndex)):
+	for j in range(3):
+		if not(i == dataBeginIndex and dataIndex == len(imageData) - 1 and charIndex == 8):
+			if charData[charIndex] == "0":
+				if flatImage[i][j] % 2 == 1:
+					pixelArr.append(flatImage[i][j] - 1)
+				else:
+					pixelArr.append(flatImage[i][j])
+			else:
+				if flatImage[i][j] % 2 == 0:
+					pixelArr.append(flatImage[i][j] + 1)
+				else:
+					pixelArr.append(flatImage[i][j])
+
+			charIndex += 1
+			if charIndex == 8 and dataIndex != len(imageData) - 1:
+				dataIndex += 1
+				charData = bin(ord(imageData[dataIndex])).lstrip("0b").zfill(8)
+				charIndex = 0
+		else:
+			pixelArr.append(flatImage[i][j])
+
+	dataArr.append((pixelArr[0],pixelArr[1],pixelArr[2]))
+	pixelArr.clear()
+
+#append remaining unchanged pixels into our data for the image.
+for i in reversed(range(0,dataBeginIndex)):
+	dataArr.append(flatImage[i])
+
+#put all data into the image.
+img.putdata(list(reversed(dataArr)))
+
+imageName = imageName.split(".")[0]
+img.save(imageName + ".png")
+img.close()
